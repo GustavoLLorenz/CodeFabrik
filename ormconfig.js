007ed40e93184526384860364330371d6a169e38
Rodrigo Brito <ramos.rbrito@gmail.com>
2023-03-23 09:53:19 -0300

    mudança versão typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,20 @@
-console.log('Configuring ORM - Conecting...')
+console.log("Configuring ORM - Conecting...");
 
 module.exports = {
-  type: 'postgres',
+  type: "postgres",
   host: process.env.TYPEORM_HOST,
   port: parseInt(process.env.TYPEORM_PORT),
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
-  schema: 'public',
+  schema: "public",
   synchronize: false,
   logging: false,
   ssl: false,
   // entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
-  migrations: ['./src/database/migrations/*.ts'],
+  migrations: ["./src/database/migrations/*.ts"],
   cli: {
     // entitiesDir: './src/modules/**/infra/typeorm/entities',
-    migrationsDir: './src/database/migrations/*.ts'
+    migrationsDir: "./src/database/migrations"
   }
-}
+};
