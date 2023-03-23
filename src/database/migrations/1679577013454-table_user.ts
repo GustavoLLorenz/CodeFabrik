3265d058cf348155f7e8c8c9aa7a9bd9cd3ae114
Rodrigo Brito <ramos.rbrito@gmail.com>
2023-03-23 10:21:41 -0300

    migration user

---
+++ src/database/migrations/1679577013454-table_user.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class tableUser1679577013454 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+    await queryRunner.createTable(
+      new Table({
+        name: "user",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()"
+          },
+          {
+            name: "user_name",
+            type: "varchar"
+          },
+          {
+            name: "cpf_cnpj",
+            type: "varchar"
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()"
+          }
+        ]
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user");
+  }
+}
